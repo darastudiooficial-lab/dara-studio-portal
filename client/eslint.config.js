45e745b36f31322b45276086f716248519c2f7f5
darastudiooficial-lab <darastudiooficial@gmail.com>
2026-06-11 11:50:23 -0300

    done: complete project audit and dead code cleanup

---
+++ client/eslint.config.js
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+import unusedImports from "eslint-plugin-unused-imports";
+import reactPlugin from "eslint-plugin-react";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        document: "readonly",
+        navigator: "readonly",
+        window: "readonly",
+        console: "readonly",
+        process: "readonly",
+        __dirname: "readonly"
+      }
+    },
+    plugins: {
+      "unused-imports": unusedImports,
+      react: reactPlugin
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+      ],
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error"
+    }
+  }
+];
