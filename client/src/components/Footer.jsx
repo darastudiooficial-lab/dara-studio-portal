a100dd1ef3b93a900f2f262f94f5e6df230fa2ca
darastudiooficial-lab <darastudiooficial@gmail.com>
2026-06-15 13:56:28 -0300

    done: update Google and WhatsApp icons in footer to match reference images

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -60,10 +60,17 @@ export default function Footer() {
           </a>
           <div className="footer-social-simple">
             <a href="https://share.google/14nAbKTvWXu3jpbDO" target="_blank" rel="noreferrer" className="footer-social-link">
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M4 12v8h16v-8"></path><path d="M12 16V2"></path></svg>
+              <svg width="18" height="18" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
+                <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
+                <path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/>
+                <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
+              </svg>
             </a>
             <a href="https://api.whatsapp.com/send/?phone=5548991234567&text&type=phone_number&app_absent=0" target="_blank" rel="noreferrer" className="footer-social-link">
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 1 1-7.6-11.7 8.38 8.38 0 0 1 3.8.9L21 3z"></path></svg>
+              <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12.01 2.01c-5.51 0-9.98 4.47-9.98 9.98 0 1.94.55 3.75 1.5 5.3L2.01 22l4.87-1.49c1.51.91 3.27 1.44 5.13 1.44 5.51 0 9.98-4.47 9.98-9.98 0-5.51-4.47-9.98-9.98-9.98zm5.55 14.37c-.24.67-1.37 1.25-1.91 1.34-.51.09-1.17.15-3.32-.73-2.58-1.05-4.24-3.7-4.37-3.87-.13-.17-1.04-1.39-1.04-2.65 0-1.26.66-1.88.89-2.13.23-.25.51-.31.68-.31s.34 0 .5-.01c.15-.01.37-.06.57.43.21.51.72 1.76.79 1.88.06.13.11.28.02.45-.08.17-.13.28-.26.43-.13.15-.28.32-.39.43-.13.13-.27.27-.13.51.15.24.66 1.07 1.41 1.74.97.87 1.78 1.13 2.02 1.26.24.13.38.11.53-.06.15-.17.65-.75.82-1.01.17-.26.34-.21.57-.13.24.08 1.51.71 1.77.84.26.13.43.21.49.33.06.13.06.74-.18 1.41z"/>
+              </svg>
             </a>
             <a href="https://www.instagram.com/darastudiooficial" target="_blank" rel="noreferrer" className="footer-social-link">
               <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
