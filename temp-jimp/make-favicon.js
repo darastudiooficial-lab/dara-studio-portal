73ca26c84ca98283fad0883b6ca50fc990ba9b49
darastudiooficial-lab <darastudiooficial@gmail.com>
2026-07-16 09:50:40 -0300

    fix: adjust favicon to just show 'D' instead of 'Da'

--- temp-jimp/make-favicon.js
+++ temp-jimp/make-favicon.js
@@ -3,15 +3,15 @@ const { Jimp } = require('jimp');
 async function run() {
   try {
     const img = await Jimp.read('../client/public/assets/logos/logo-horizontal.png');
-    // Crop the leftmost portion that contains just the icon
-    // Height is 255, so we take a 270x255 chunk from the left
-    img.crop({ x: 0, y: 0, w: 270, h: 255 });
-    // Autocrop any extra transparent borders
+    // Crop only the 'D' part (width ~140, height 255)
+    img.crop({ x: 0, y: 0, w: 140, h: 255 });
+    // Autocrop extra transparent borders
     img.autocrop();
-    // Resize to a square 64x64 for favicon
-    img.resize({ w: 64, h: 64 });
+    // Contain within a 64x64 box to keep proportions and add transparent padding if needed
+    img.contain({ w: 64, h: 64 });
+    
     await img.write('../client/public/favicon.png');
-    console.log('Favicon created successfully');
+    console.log('Favicon D created successfully');
   } catch (err) {
     console.error(err);
   }
